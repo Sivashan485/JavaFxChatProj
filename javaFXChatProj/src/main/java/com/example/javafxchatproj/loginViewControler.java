2ce52d1624e10c32750ba5405ba931a5243ea128
sivaksiv <sivaksiva@students.zhaw.ch>
2024-02-23 21:50:41 +0100

    login VIEW function unsafe

--- javaFXChatProj/src/main/java/com/example/javafxchatproj/loginViewControler.java
+++ javaFXChatProj/src/main/java/com/example/javafxchatproj/loginViewControler.java
@@ -1,14 +1,44 @@
 package com.example.javafxchatproj;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class loginViewControler {
+
+    String username = "ASDF";
+    String pwd = "ASDF";
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFXChat!");
+    private TextField TfUsername;
+
+    @FXML
+    private PasswordField PfPassword;
+
+
+
+    public void onClickLoginView(ActionEvent actionEvent) {
+        if(checkLogin()){
+
+        }else{
+            createFailedLoginMessage();
+        }
+    }
+
+    private boolean checkLogin(){
+        return (TfUsername.getText().equals(username) && PfPassword.getText().equals(pwd));
+    }
+
+    private void createFailedLoginMessage(){
+        Alert loginFailedMessage = new Alert(Alert.AlertType.WARNING);
+        loginFailedMessage.setTitle("JAVAFXCHAT - ERROR");
+        loginFailedMessage.setHeaderText("SOMETHING IS WRONG WITH THE LOGIN PROCEDURE");
+        loginFailedMessage.setContentText("Login failed, pls try again!!!");
+        loginFailedMessage.show();
     }
 }
