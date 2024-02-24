ee02bdf01601f426c8c0d316cf9c0fdfbdb4c229
sivaksiv <sivaksiva@students.zhaw.ch>
2024-02-24 14:04:07 +0100

    minor design changes on login view

--- javaFXChatProj/src/main/java/com/example/javafxchatproj/loginView.java
+++ javaFXChatProj/src/main/java/com/example/javafxchatproj/loginView.java
@@ -11,7 +11,9 @@ public class loginView extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(loginView.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 300, 400);
+        scene.getStylesheets().add(loginView.class.getResource("Login.css").toExternalForm());
+
 
         stage.setTitle("JavaFXCHAT LOGIN");
         stage.setScene(scene);
